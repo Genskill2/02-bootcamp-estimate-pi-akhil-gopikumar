6296a1dcae614c0f87edb45058d125901eeacef1
akhil-gopikumar <akhilgk01@gmail.com>
2021-06-01 10:16:38 +0530

    Initial version of program. Two functions added, namely villis and monte_carlo

--- estimate.py
+++ estimate.py
@@ -1,12 +1,29 @@
 import math
 import unittest
+import random
+
+def wallis(n):
+    prod = 1
+    for i in range(1,n):
+        prod = prod*(4*(i**2))/(4*(i**2)-1)
+    return prod*2
+
+def monte_carlo(n):
+    count_inside = 0
+    for i in range(n):
+        x = random.random()
+        y = random.random()
+        if math.sqrt(x**2 + y**2) > 0.5:
+            count_inside = count_inside + 1
+    pi = 4*count_inside/n
+    return pi
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
+
     def test_high_iters(self):
         for i in range(500, 600):
             pi = wallis(i)
@@ -17,7 +34,7 @@ class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
+
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
@@ -26,7 +43,7 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+
+
 if __name__ == "__main__":
     unittest.main()
